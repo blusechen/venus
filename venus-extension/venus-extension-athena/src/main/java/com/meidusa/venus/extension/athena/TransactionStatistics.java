669afd212b15f13bbe30384cc722d2856a544bd4
GodzillaHua <godzillahua@gmail.com>
2016-08-16 11:57:42 +0800

    统计流量接口

---
+++ venus-extension/venus-extension-athena/src/main/java/com/meidusa/venus/extension/athena/TransactionStatistics.java
@@ -0,0 +1,12 @@
+package com.meidusa.venus.extension.athena;
+
+/**
+ * Created by GodzillaHua on 7/25/16.
+ */
+public interface TransactionStatistics {
+
+    void setInputSize(long size);
+
+    void setOutputSize(long size);
+
+}
