22661ea2915e38bad20b8c695bc3f29889b0812b
Administrator <Administrator@hexnova>
2023-03-01 22:11:44 +0800

    fixed ClasspathAnnotationScanner

--- venus-commons/venus-common-base/src/main/java/com/meidusa/venus/util/ClasspathAnnotationScanner.java
+++ venus-commons/venus-common-base/src/main/java/com/meidusa/venus/util/ClasspathAnnotationScanner.java
@@ -1,11 +1,18 @@
 package com.meidusa.venus.util;
 
 import java.lang.annotation.Annotation;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 
 public class ClasspathAnnotationScanner {
     private ClasspathAnnotationScanner(){}
@@ -18,11 +25,18 @@ public class ClasspathAnnotationScanner {
      */
     public static <K,T extends Annotation> Map<Class<?>,T> find(Class<K> parent,Class<T> targetAnnotation){
     	
-    	return find(parent,targetAnnotation,null);
+    	return find(parent,targetAnnotation,"");
     }
     
-    public static <K,T extends Annotation> Map<Class<?>,T> find(Class<K> parent,Class<T> targetAnnotation,String packageName){
-    	Reflections reflections = new Reflections(packageName);
+    public static <K,T extends Annotation> Map<Class<?>,T> find(Class<K> parent,Class<T> targetAnnotation,String... packageName){
+    	Collection<URL> urls = new ArrayList<URL>();
+    	if(packageName != null && packageName.length > 0) {
+    		for(String name : packageName) {
+    			urls.addAll(ClasspathHelper.forPackage(name));
+    		}
+    	}
+    	
+    	Reflections reflections = new Reflections(urls,Scanners.TypesAnnotated);
     	
         Set<Class<?>> annotated =  reflections.getTypesAnnotatedWith(targetAnnotation);
         
--- venus-commons/venus-common-exception/src/test/java/Taaa.java
+++ venus-commons/venus-common-exception/src/test/java/Taaa.java
@@ -1,4 +1,13 @@
 import java.util.Map;
+import java.util.Set;
+
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
 
 import com.meidusa.venus.annotations.RemoteException;
 import com.meidusa.venus.util.ClasspathAnnotationScanner;
@@ -8,12 +17,20 @@ public class Taaa {
 
 	public static void main(String[] args) {
     	long start = System.currentTimeMillis();
-    	Map<Class<?>,RemoteException> map = ClasspathAnnotationScanner.find(Exception.class, RemoteException.class,"com.meidusa.venus");
+    	Map<Class<?>,RemoteException> map = ClasspathAnnotationScanner.find(Exception.class, RemoteException.class,"");
     	
     	for(Map.Entry<Class<?>,RemoteException> entry : map.entrySet()){
     		System.out.println(entry.getKey());
     	}
     	System.out.println(System.currentTimeMillis() - start);
+    	
+    	Reflections reflections = new Reflections(ClasspathHelper.forPackage("com.meidusa.venus"),ClasspathHelper.forPackage("com.hexnova"));
+    	
+        Set<Class<?>> annotated =  reflections.getTypesAnnotatedWith(RemoteException.class);
+        
+        for(Class<?> entry : annotated){
+    		System.out.println(entry);
+    	}
 	}
 
 }
